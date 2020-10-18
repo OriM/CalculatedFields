b5be329205750a5b684efc140f509c99d9722c7d
Ori Millo <ori.m@pepperi.com>
2020-10-18 21:34:42 +0300

    Prettier

--- bold_price.js
+++ bold_price.js
@@ -1,9 +1,10 @@
-var html = '<p class="body-md" style="text-align : right;"><strong>{{Currency}}{{ItemPrice}}</strong></p>';
+var html =
+  '<p class="body-md" style="text-align : right;"><strong>{{Currency}}{{ItemPrice}}</strong></p>';
 
 var currency = GetValueByApiName("Transaction.Currency");
 
 var res = html;
-res = res.replace("{{ItemPrice}}",ItemPrice); // ItemPrice is in the Available Fields
-res = res.replace("{{Currency}}",currency);
+res = res.replace("{{ItemPrice}}", ItemPrice); // ItemPrice is in the Available Fields
+res = res.replace("{{Currency}}", currency);
 
-return res; 
+return res;
