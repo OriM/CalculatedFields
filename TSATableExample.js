e517c62e4f02a7f820340760ec4255f0b9467747
Ori Millo <ori.m@pepperi.com>
2021-06-07 14:05:23 +0300

    added style to table

--- TSATableExample.js
+++ TSATableExample.js
@@ -1,4 +1,23 @@
-var res = `<table>
+var res = `
+<style>
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+
+tr:nth-child(even) {
+  background-color: #dddddd;
+}
+</style>
+
+<table>
 <tr>
   <th>Company</th>
   <th>Contact</th>
