c28d084f43677838b23f7869e5f601858b06d8bb
Ori Millo <ori.m@pepperi.com>
2021-06-07 13:29:32 +0300

    added Table Example

---
+++ TSATableExample.js
@@ -0,0 +1,45 @@
+var res = `<table>
+<tr>
+  <th>Company</th>
+  <th>Contact</th>
+  <th>Country</th>
+  <th>How Boss Is Yonatan</th>
+</tr>
+<tr>
+  <td>Alfreds Futterkiste</td>
+  <td>Maria Anders</td>
+  <td>Germany</td>
+  <td>Very Much</td>
+</tr>
+<tr>
+  <td>Centro comercial Moctezuma</td>
+  <td>Francisco Chang</td>
+  <td>Mexico</td>
+  <td>Extremely</td>
+</tr>
+<tr>
+  <td>Ernst Handel</td>
+  <td>Roland Mendel</td>
+  <td>Austria</td>
+  <td>Like Never Seen Before</td>
+</tr>
+<tr>
+  <td>Island Trading</td>
+  <td>Helen Bennett</td>
+  <td>UK</td>
+  <td>God Level</td>
+</tr>
+<tr>
+  <td>Laughing Bacchus Winecellars</td>
+  <td>Yoshi Tannamuri</td>
+  <td>Canada</td>
+  <td>To The Moon</td>
+</tr>
+<tr>
+  <td>Magazzini Alimentari Riuniti</td>
+  <td>Giovanni Rovelli</td>
+  <td>Italy</td>
+  <td>Game Boss Level</td>
+</tr>
+</table>`;
+return res;
